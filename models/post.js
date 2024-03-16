1e6f34545519dcd81dd17b4ae8bb8ae1a63bad20
ellaYang1227 <49946780+ellaYang1227@users.noreply.github.com>
2024-03-16 15:01:33 +0800

    修正 createdAt 的 Schema default 值設定錯誤

--- models/post.js
+++ models/post.js
@@ -37,7 +37,7 @@ const postSchema = new mongoose.Schema(
         },
         createdAt: {
             type: Date,
-            default: Date.now(),
+            default: Date.now,
             select: false
         },
         type: {
